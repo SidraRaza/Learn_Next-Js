72d0274312c2a2c97216c84f6b00d244930012bd
SidraRaza <160983959+SidraRaza@users.noreply.github.com>
2024-08-16 17:38:28 +0500

    first commit

---
+++ Hello World/hello/src/app/page.tsx
@@ -0,0 +1,11 @@
+
+
+export default function Home() {
+  return (
+    <main>
+      <div>
+        <h1 className="text-3xl font-bold text-center py-20">Hello, World!</h1>
+      </div>
+    </main>
+  );
+}
