0a3a35e81c0f3572bf40384ab2113552148be860
SidraRaza <160983959+SidraRaza@users.noreply.github.com>
2024-08-17 14:17:17 +0500

    first commit

---
+++ Font Optimizattion/font/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+   <h1 className="font-serif-regular text-2xl font-italic  text-red-500 text-center pt-20">Font Optimization In Next Js</h1>
+   <h1 className="font-mono text-2xl font-bold  text-blue-500 text-center pt-5">Font With Link Tag In Next Js</h1>
+    </main>
+  );
+}
