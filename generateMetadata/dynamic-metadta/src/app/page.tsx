88d2d1ac366153a200ab086db86f5bfe59813330
SidraRaza <160983959+SidraRaza@users.noreply.github.com>
2024-08-21 11:53:54 +0500

    first commit

---
+++ generateMetadata/dynamic-metadta/src/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+<div>
+  <h1 className="text-3xl font-bold text-center text-red-500">
+    Generate Mete Data
+  </h1>
+</div>
+    </main>
+  );
+}
