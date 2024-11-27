e9fbc8d3c183653d2d1007b737388a8a73610f9f
SidraRaza <160983959+SidraRaza@users.noreply.github.com>
2024-11-27 15:30:25 +0500

    first commit

---
+++ Backened/delete-api/src/app/user/page.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import Link from 'next/link'
+
+async function getData() {
+    let data=await fetch("http://localhost:3000/api/users")
+    data=await data.json();
+    return data
+}
+async function page() {
+    const users = await getData();
+    console.log(users);
+  
+   
+    
+    return (
+      <>
+        <div className='text-center text-3xl py-10'>User page</div>
+        {
+          users.map((item: any) => (
+            <div className='flex justify-center items-center ' key={item.name}>
+             <span><Link href={`/user/${item.id}`}>{item.name}</Link></span>
+             <span className='ml-2 text-red-600'><Link href={`/user/${item.id}/update`}>Edit</Link></span>
+             
+            </div>
+          ))
+        }
+      </>
+    );
+  };
+  
+  export default page;
