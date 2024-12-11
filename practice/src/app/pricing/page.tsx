cd20081bc80777e3e7a13d5173d010e4087d4e8a
SidraRaza <160983959+SidraRaza@users.noreply.github.com>
2024-12-11 11:32:42 +0500

    first commit

---
+++ practice/src/app/pricing/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='w-[870px] h-[280px] flex justify-center items-center mx-auto'>
+        <div className='w-[427px] h-[180px] flex flex-col justify-center items-center mx-auto'>
+      <h1 className='text-[16px] font-bold leading-[24px]'>pricing</h1>
+    </div>
+    </div>
+  )
+}
+
+export default page
