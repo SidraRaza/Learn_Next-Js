fdb9894b2761324bc9f2fa6256b50363d0d9bae2
SidraRaza <160983959+SidraRaza@users.noreply.github.com>
2024-12-11 15:33:57 +0500

    first commit

--- gsap/create-animation/src/app/page.tsx
+++ gsap/create-animation/src/app/page.tsx
@@ -10,18 +10,19 @@ export default function AnimatedBox() {
   
   // Using useGSAP to run animation on mount
   useGSAP(() => {
-    gsap.to(boxRef.current, { x: 700, duration: 3,borderRadius: "50%" }
-    );
-    gsap.from(boxRef2.current, { y: 700, duration: 3, delay: 1,rotate: 360,backgroundColor: "green",
-      borderRadius: "50%"
+    // gsap.to(boxRef.current, { x: 700, duration: 3,borderRadius: "50%" }
+    // );
+    // gsap.from(boxRef2.current, { y: 700, duration: 3, delay: 1,rotate: 360,backgroundColor: "green",
+    //   borderRadius: "50%"
 
-     }
-    );
+    //  }
+    // );
+    gsap.from([boxRef.current, boxRef2.current],{ opacity: 0, duration: 3 ,rotate: 360,x:700,borderRadius: "50%",yoyo: true,delay:3,stagger:0.5,repeat:1});
   }, []);
 
   return <div  >
- <h1 ref={boxRef} className="w-[200px] h-[200px] bg-red-500 flex justify-center items-center p-5 text-white">   I'm animated!</h1>
- <h2 ref={boxRef2} className="w-[200px] h-[200px] bg-yellow-500 flex justify-center items-center p-5 text-white">I'm animated!</h2>
+ <h1 ref={boxRef} className="text-3xl font-bold bg-red-700 p-10 w-[300px]">   I'm animated!</h1>
+ <h2 ref={boxRef2} className="text-3xl font-bold">I'm animated!</h2>
   
   </div>;
 }
